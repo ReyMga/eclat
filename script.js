49f1c0e7540f2a0e097a7e167cc2be711b6dd7b8
Gustavo Sanchez <41483568+GSanchez00@users.noreply.github.com>
2024-06-04 15:22:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const creatures = document.querySelectorAll('.creatures span');
+    const images = document.querySelectorAll('.carousel-slide img');
+    
+    creatures.forEach(creature => {
+        creature.addEventListener('click', function () {
+            const id = 'img-' + this.id;
+            images.forEach(img => {
+                if (img.id === id) {
+                    img.style.display = 'block';
+                } else {
+                    img.style.display = 'none';
+                }
+            });
+        });
+    });
+});
